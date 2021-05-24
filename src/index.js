4f00ad82b02e2ff692201abea5406010ba167f72
Klipach <klipach@mail.ru>
2021-05-24 18:00:46 +0300

    var1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MaskInput from './MaskInput';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MaskInput />
   </React.StrictMode>,
   document.getElementById('root')
 );
